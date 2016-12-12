e6fa1580b68278654ef1624b464cd136f741777a
Angela Rumpl <angela.rumpl@kununu.com>
2016-12-12 17:59:03 +0100

    Add pact broker

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-First try of using pact (setup from: https://github.com/andykelk/pact-php)
+## First try of using pact (setup from: https://github.com/andykelk/pact-php)
 
 1. Clone the repo
 2. Install pact mock service:
@@ -7,5 +7,22 @@ First try of using pact (setup from: https://github.com/andykelk/pact-php)
 3. Install composer: curl -sS https://getcomposer.org/installer | php
 4. Install dependencies: php composer.phar install
 5. Start the mock server: `bundle exec pact-mock-service -p 1234 --pact-specification-version 2.0.0 -l log/pact.logs --pact-dir tmp/pacts`
-6. Run phpunit: `./vendor/bin/phpunit`
+6. Run phpunit: `./vendor/bin/phpunit --bootstrap=vendor/autoload.php tests/`
 7. Inspect the pact file in `tmp/pacts`
+
+
+
+## Setup Pact Broker
+
+
+Easily setup your own [Pact Broker](https://github.com/bethesque/pact_broker)
+instance with docker-compose.
+
+run
+
+        docker-compose pull
+        docker-compose up -d db
+        docker-compose up
+
+Done! You can configure your Pact Broker and Postgres with the environment
+variables inside the docker-compose.yml file.
--- tests/MyProviderTest.php
+++ tests/MyProviderTest.php
@@ -1,5 +1,6 @@
 <?php
 
+
 use Pact\Pact;
 use ZooConsumer\ZooClient;
 
@@ -8,9 +9,11 @@ class AlligatorProviderTest extends PHPUnit_Framework_TestCase
     public function testMyProvider() {
         $client = new ZooClient('http://localhost:1234');
 
+        $consumerName = 'Alligator Consumer';
+        $providerName = 'Alligator Provider';
         $alligatorProvider = Pact::mockService([
-            'consumer' => 'Alligator Consumer',
-            'provider' => 'Alligator Provider',
+            'consumer' => $consumerName,
+            'provider' => $providerName,
             'port' => 1234
         ]);
 
@@ -30,6 +33,13 @@ class AlligatorProviderTest extends PHPUnit_Framework_TestCase
             $this->assertInstanceOf(\ZooConsumer\Alligator::class, $alligator);
             $this->assertEquals("Mary", $alligator->getName());
         });
+
+        //post the contract to pact broker
+
+        $alligatorPact =  realpath(__DIR__ . '/../tmp/pacts/alligator_consumer-alligator_provider.json');
+        $client  = new \Http\Adapter\Guzzle6\Client();
+        $client = new \Madkom\PactBrokerClient\HttpBrokerClient('127.0.0.1:5000', $client, new \Madkom\PactBrokerClient\RequestBuilder());
+        $response = $client->publishPact($providerName, $consumerName, '1.0.5', $alligatorPact);
     }
 
 }
